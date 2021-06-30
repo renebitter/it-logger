04c42274ec399d46027d0dd6fc336c1b6a8cd323
Rene <renebitter@gmail.com>
2021-06-30 18:49:18 -0300

    Delete Logs
    action, reducer, component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+} from "./types";
 
 // Get logs from server
 export const getLogs = () => async (dispatch) => {
@@ -20,7 +26,7 @@ export const getLogs = () => async (dispatch) => {
   }
 };
 
-// Adds log to server
+// Adds log
 export const addLog = (log) => async (dispatch) => {
   try {
     setLoading();
@@ -37,6 +43,27 @@ export const addLog = (log) => async (dispatch) => {
     dispatch({
       type: ADD_LOG,
       payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// Deletes log
+export const deleteLog = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/logs/${id}`, {
+      method: "DELETE",
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id,
     });
   } catch (err) {
     dispatch({
