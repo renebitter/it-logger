151448f28c13112cc025b7c5a9584346a682dc45
Rene <renebitter@gmail.com>
2021-06-30 11:42:06 -0300

    Redux Store & Provider Setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from "react";
+import { useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
@@ -6,6 +6,8 @@ import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -17,7 +19,7 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <>
+    <Provider store={store}>
       <SearchBar />
       <div className="container">
         <AddBtn />
@@ -27,7 +29,7 @@ const App = () => {
         <TechListModal />
         <Logs />
       </div>
-    </>
+    </Provider>
   );
 };
 
