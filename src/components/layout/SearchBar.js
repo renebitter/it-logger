46d83b088319ab3984caf843acabeb41995505c5
Rene <renebitter@gmail.com>
2021-07-01 10:31:06 -0300

    Search logs

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,10 +1,27 @@
-const SearchBar = () => {
+import { useRef } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { searchLogs } from "../../actions/logActions";
+
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef("");
+
+  const onChange = (e) => {
+    searchLogs(text.current.value);
+  };
+
   return (
     <nav style={{ marginBottom: "30px" }} className="blue">
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input id="search" type="search" required />
+            <input
+              id="search"
+              type="search"
+              placeholder="Search here..."
+              ref={text}
+              onChange={onChange}
+            />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
@@ -16,4 +33,8 @@ const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired,
+};
+
+export default connect(null, { searchLogs })(SearchBar);
